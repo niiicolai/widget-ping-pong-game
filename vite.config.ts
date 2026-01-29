3ae76d97ab56d0c0c47a40e19bf9388069b22910
Nicolai <niiicolai@gmail.com>
2026-01-29 07:34:17 +0100

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/widget-github-user-tracker",
+  base: "/widget-ping-pong-game",
   plugins: [tailwindcss(), react()],
 });
